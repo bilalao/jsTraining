9441c8058b4daf5b6cf747b3f5e630b077f0fe72
Broadseg <broadseg@Broadsegs-iMac.local>
2020-12-26 12:55:03 +0100

    Js training

---
+++ capitalize.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/*
+ * Create a function `capitalize` takes a string
+ * and transform to upper case only the first letter
+ *
+ * @next jaden-case
+ */
+
+// You must write your own tests
+throw Error('No tests !')
